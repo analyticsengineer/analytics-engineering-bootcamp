d31c65dde18d193661fbc8e7ea1fa972870307a9
analyticsengineer <anuoluwapobalogunds@gmail.com>
2024-04-02 22:47:53 +0100

    Created AE Project

---
+++ dbt-env/ae_bootcamp/models/analytics_obt/obt_product_inventory.sql
@@ -0,0 +1,30 @@
+with source as (
+    select
+        i.inventory_id,
+        i.transaction_type,
+        i.transaction_created_date,
+        i.transaction_modified_date,
+        i.product_id as ipd,
+        i.quantity,
+        i.purchase_order_id,
+        i.customer_order_id,
+        i.comments,
+        p.product_id,
+        p.product_code,
+        p.product_name,
+        p.description,
+        p.supplier_company,
+        p.standard_cost,
+        p.list_price,
+        p.reorder_level,
+        p.target_level,
+        p.quantity_per_unit,
+        p.discontinued,
+        p.minimum_reorder_quantity,
+        p.category
+    from {{ ref('fact_inventory') }} i
+      join {{ ref('dim_product') }} p
+    on p.product_id = i.product_id
+)
+select * 
+from source
---
+++ dbt-env/ae_bootcamp/models/warehouse/fact_inventory.sql
@@ -0,0 +1,32 @@
+WITH source AS (
+    SELECT
+        id AS inventory_id,
+        transaction_type,
+        DATE(transaction_created_date) AS transaction_created_date,
+        transaction_modified_date,
+        product_id,
+        quantity,
+        purchase_order_id,
+        customer_order_id,
+        comments,
+        CURRENT_TIMESTAMP() AS insertion_timestamp
+    FROM {{ ref('stg_inventory_transactions') }}
+),
+unique_source AS (
+    SELECT *,
+            ROW_NUMBER() OVER(PARTITION BY inventory_id) AS row_number
+    FROM source
+)
+SELECT 
+    inventory_id,
+    transaction_type,
+    transaction_created_date,
+    transaction_modified_date,
+    product_id,
+    quantity,
+    purchase_order_id,
+    customer_order_id,
+    comments,
+    insertion_timestamp
+FROM unique_source
+WHERE row_number = 1
